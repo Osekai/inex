453b163ee8f4ef80c4e952d200b13f6957d23d7e
Tanza3D <archie@untone.uk>
2024-09-24 16:12:46 +0100

    transitionable bg

--- views/medals.php
+++ views/medals.php
@@ -4,38 +4,41 @@
 
         </div>
     </div>
-    <div class="main col-reset" style="--hue: 327" id="main-col">
-        <div class="main-left">
-            <div class="panel">
-                <h1>Info</h1>
-                <div class="divider"></div>
-                <img id="medal_image" src="https://assets.ppy.sh/medals/web/taiko-hits-30000.png">
-                <h1 id="medal_name">Medal Name</h1>
-                <h2 id="medal_description">Medal Hint</h2>
-                <h3 id="medal_instructions">Medal Subhint</h3>
-                <div class="button cta" id="medal-edit-button">Open Editor</div>
-            </div>
-
-            <div class="panel">
-                <h1>Solution</h1>
-                <div class="divider"></div>
-                <p id="medal_solution">Solution</p>
-            </div>
-            <div class="panel">
-                <div class="button-row">
-                    <h1>Beatmaps</h1>
-                    <button id="medal_beatmaps_add" class="button cta pill-button">Add Beatmap</button>
+    <div class="main col-reset" id="main-col">
+        <div class="bg"></div>
+        <div class="scrollable">
+            <div class="main-left">
+                <div class="panel">
+                    <h1>Info</h1>
+                    <div class="divider"></div>
+                    <img id="medal_image" src="https://assets.ppy.sh/medals/web/taiko-hits-30000.png">
+                    <h1 id="medal_name">Medal Name</h1>
+                    <h2 id="medal_description">Medal Hint</h2>
+                    <h3 id="medal_instructions">Medal Subhint</h3>
+                    <div class="button cta" id="medal-edit-button">Open Editor</div>
                 </div>
-                <div class="divider"></div>
-                <div id="medal_beatmaps">
 
+                <div class="panel">
+                    <h1>Solution</h1>
+                    <div class="divider"></div>
+                    <p id="medal_solution">Solution</p>
+                </div>
+                <div class="panel">
+                    <div class="button-row">
+                        <h1>Beatmaps</h1>
+                        <button id="medal_beatmaps_add" class="button cta pill-button">Add Beatmap</button>
+                    </div>
+                    <div class="divider"></div>
+                    <div id="medal_beatmaps">
+
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="main-right">
-            <div class="panel">
-                <h1>Comments</h1>
-                <div class="divider"></div>
+            <div class="main-right">
+                <div class="panel">
+                    <h1>Comments</h1>
+                    <div class="divider"></div>
+                </div>
             </div>
         </div>
     </div>
