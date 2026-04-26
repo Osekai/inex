49604194ba65f637265c4dedd8d95269a74ca80d
Tanza3D <archie@untone.uk>
2026-04-26 21:59:07 +0100

    ok

--- database/migrations/20260426205738_indexes_for_graphs.php
+++ database/migrations/20260426205738_indexes_for_graphs.php
@@ -23,5 +23,32 @@ final class IndexesForGraphs extends AbstractMigration
         $table->addIndex(['Achieved_At'], ['name' => 'idx_achieved_at'])
             ->addIndex(['User_ID', 'Achieved_At'], ['name' => 'idx_user_achieved_at'])
             ->update();
+
+        $table = $this->table('Medals_Configuration');
+        $table->addIndex(['Date_Released'], ['name' => 'idx_date_released'])
+            ->update();
+
+        $table = $this->table('Common_Comments');
+        $table->addIndex(['Target_ID', 'Target_Table'], ['name' => 'idx_target'])
+            ->addIndex(['User_ID'], ['name' => 'idx_user_id'])
+            ->addIndex(['Parent_Comment_ID'], ['name' => 'idx_parent_comment'])
+            ->update();
+
+        $table = $this->table('Common_Votes');
+        $table->addIndex(['User_ID'], ['name' => 'idx_user_id'])
+            ->update();
+
+        $table = $this->table('Medals_Beatmaps');
+        $table->addIndex(['Medal_ID'], ['name' => 'idx_medal_id'])
+            ->addIndex(['Beatmap_ID'], ['name' => 'idx_beatmap_id'])
+            ->update();
+
+        $table = $this->table('Badges_Users');
+        $table->addIndex(['User_ID'], ['name' => 'idx_user_id'])
+            ->update();
+
+        $table = $this->table('System_Roles_Assignments');
+        $table->addIndex(['Role_ID'], ['name' => 'idx_role_id'])
+            ->update();
     }
 }
